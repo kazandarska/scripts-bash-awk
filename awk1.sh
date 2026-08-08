e1db596e12d14babf3f6e2c8139301a269b5c4ae
Katarzyna Zandarska <314211569+kazandarska@users.noreply.github.com>
2026-08-08 23:21:37 +0200

    Add files via upload

---
+++ awk1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+plik="dane.txt"
+if [ -f $plik ]; then
+awk '{sum=0; for(i=1; i<=NF; i++) sum+=$i; srednia=sum/NF; printf "srednia wiersza: %.2f\n", srednia;
+
+if (srednia > 30) {print "wartosc powyzej oczekiwanej"}
+else if (srednia > 20) {print "wartosc graniczna"}
+else {print "wartosc ponizej oczekiwanej"} }' $plik
+else 
+echo "Plik $plik nie istnieje"
+fi
